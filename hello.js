9845295c025d72f5deb66b26d01c4a978700a320
Ricardo Amaro <mail@ricardoamaro.com>
2018-11-06 12:57:02 +0000

    added javascript example

---
+++ hello.js
@@ -0,0 +1,13 @@
+/*
+ * Requires: http://nodejs.org/#download or run in a browser
+ * Usage: node hello.js 
+ */
+
+const x = 'Hello'
+const y = 'World'
+
+function hello_console() {
+  console.log(x, y);
+}
+
+hello_console(x, y);
