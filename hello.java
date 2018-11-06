591d181b4ff8379b5dd7ac19435e3fa062d7cb5e
Ricardo Amaro <mail@ricardoamaro.com>
2018-11-06 17:04:02 +0000

    added Java

---
+++ hello.java
@@ -0,0 +1,14 @@
+/*
+ * 'Hello World!' program in Java
+ * Requires: java
+ * Compile:  javac hello.java
+ * Run:      java hello
+ */
+
+public class hello
+{
+  public static void main(String[] args)
+    {
+      System.out.println("Hello world!");
+    }
+}
