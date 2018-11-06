5f5116e900bd68d7829715ab6fa49c486b86c50b
Ricardo Amaro <mail@ricardoamaro.com>
2018-11-06 17:08:24 +0000

    added Lua

---
+++ hello.lua
@@ -0,0 +1,11 @@
+--[[
+  'Hello World!' program in Java
+  Requires: lua (https://www.lua.org/download.html)
+  Compile:
+  Run:      lua hello.lua
+]]--
+
+x = "Hello "
+y = "World!"
+
+print (x..y);
