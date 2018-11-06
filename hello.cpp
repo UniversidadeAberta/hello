edd63dc6ce7bce2f5bf168dd2de75f072ffa7ecb
Ricardo Amaro <mail@ricardoamaro.com>
2018-11-06 13:36:39 +0000

    added CPP and updated C

--- hello.cpp
+++ hello.cpp
@@ -1,18 +1,18 @@
 /*
- * 'Hello World!' program in C
- * Requires: gcc
- * Compile:  gcc -g -Wall -o hello hello.cpp
+ * 'Hello World!' program in C++
+ * Requires: g++ gcc
+ * Compile:  g++ -g -Wall -o hello hello.cpp
  * Run:      ./hello
  * On Ubuntu/Linux just install gcc with apt
  * On Centos/Redhat/Fedore install with yum/dnf
  * On Windows download from http://www.mingw.org/
  * On a Mac use brew install
  */
-
-#include <stdio.h>
-
-int main(void)
+ 
+#include <iostream>
+ 
+int main()
 {
-  printf ("Hello World!\n");
+  std::cout << "Hello World!" << std::endl;
   return 0;
 }
